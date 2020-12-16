b1495a81beabc10e45f19004e9db77f020388cd2
8bit-nyk <nicolasabboud96@gmail.com>
2020-12-16 17:20:26 +0200

    Created Menu, done color picker and fixed buttons. Created slider for pixel count instead of reset button

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
-const bwButton = document.getElementById("bwBtn");
 const grayscaleButton = document.getElementById("grayscaleBtn");
 const rainbowButton = document.getElementById("rainbowBtn");
 const eraserButton = document.getElementById("eraserBtn");
 const clearButton = document.getElementById("clearBtn");
-const resetButton = document.getElementById('resetBtn');
 const container = document.getElementById('container');
+const slider = document.getElementById("pxRange");
+const pxCount = document.getElementById("pxCount");
+const colorSelection= document.getElementsByClassName("colorItem");
 
 
 
-bwButton.addEventListener('click', bwGrid );
-resetButton.addEventListener('click', resetGrid );
 clearButton.addEventListener('click', clearGrid );
 rainbowButton.addEventListener('click', rainbowGrid );
 grayscaleButton.addEventListener('click', grayscaleGrid );
 eraserButton.addEventListener('click', eraserBrush );
+for( let i = 0; i < colorSelection.length; i++)
+  { 
+    colorSelection[i].addEventListener('click',changeBrush);
+  }
 
-function bwGrid(){
+pxCount.innerHTML += slider.value;
+createGrid(slider.value);
+slider.oninput = function() {
+  pxCount.innerHTML = "Pixel Count: "+this.value;
+  document.querySelectorAll('.box').forEach( element => {
+    element.remove();
+  });
+  createGrid(this.value);
+}
+
+function changeBrush(e){
+  //Reset selected color
+  for( let i = 0; i < colorSelection.length; i++)
+  { 
+    colorSelection[i].style.color = "white";
+    colorSelection[i].style.backgroundColor = "red";
+
+  }
+
+  //Highlight new selected color
+  this.style.color = this.innerHTML;
+  this.style.backgroundColor = 'white';
   document.querySelectorAll('.box').forEach(element => {
     element.addEventListener('mouseover',event =>{
-      element.style.backgroundColor = "black";
+      element.style.backgroundColor = this.innerHTML; 
     })
-    
+      
   });
+  
 }
+
 function grayscaleGrid(){
   document.querySelectorAll('.box').forEach(element => {
       element.addEventListener('mouseover',event =>{
+        element.style.backgroundColor = "black"; 
+
         let bgColor = element.style.getPropertyValue("background-color");
         console.log(bgColor);
        /* red = Math.floor(Math.random() * 256);
@@ -36,7 +64,10 @@ function grayscaleGrid(){
       
     });
 
-}
+  }
+
+
+
 function rainbowGrid() {
   let red, green, blue;
 
@@ -67,6 +98,9 @@ function clearGrid() {
       element.style.backgroundColor = "white";
      });
 }
+
+
+/*
 function resetGrid() {
   let userGridside = window.prompt("Enter the gride size you want.\nFor example for a 16x16 grid enter 16.\n(The number should be less than 100)");
   if (isNaN(userGridside))
@@ -88,6 +122,10 @@ function resetGrid() {
   }  
 
 }
+*/
+
+
+
 
 function createGrid(side){
     container.style.setProperty('--grid-rows', side);
@@ -116,10 +154,6 @@ function createGrid(side){
 
 
 
-
-
-
-createGrid(80);
 
 
 
