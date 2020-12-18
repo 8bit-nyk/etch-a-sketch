c8f0bcb7395e1482cf0f687b904f2d095a03b3c4
8bit-nyk <nicolasabboud96@gmail.com>
2020-12-18 03:44:29 +0200

    Refactored code into easier logic and format Finished Website

--- script.js
+++ script.js
@@ -2,20 +2,46 @@ const grayscaleButton = document.getElementById("grayscaleBtn");
 const rainbowButton = document.getElementById("rainbowBtn");
 const eraserButton = document.getElementById("eraserBtn");
 const clearButton = document.getElementById("clearBtn");
+const toggleButton = document.getElementById("toggleBtn");
 const container = document.getElementById('container');
 const slider = document.getElementById("pxRange");
 const pxCount = document.getElementById("pxCount");
 const colorSelection= document.getElementsByClassName("colorItem");
+const colorIndicator = document.getElementById("colorIndicator");
+let toggleFlag = false;
+let userChoice = "black";
 
 
 
 clearButton.addEventListener('click', clearGrid );
 rainbowButton.addEventListener('click', rainbowGrid );
-grayscaleButton.addEventListener('click', grayscaleGrid );
+grayscaleButton.addEventListener('click', grayscaleGrid);
 eraserButton.addEventListener('click', eraserBrush );
+toggleButton.addEventListener('click', toggleGridlines);
+
+
+
 for( let i = 0; i < colorSelection.length; i++)
   { 
-    colorSelection[i].addEventListener('click',changeBrush);
+    let colorS = colorSelection[i];
+    colorS.addEventListener('click',() => {
+      colorIndicator.style.backgroundColor = colorS.innerHTML;
+      userChoice =  colorS.innerHTML;
+    
+      }
+      );
+    colorS.addEventListener('mouseover',() => {
+          colorS.style.backgroundColor = "white";
+          colorS.style.color = colorS.innerHTML;
+      }
+      );
+    colorS.addEventListener('mouseout',() => {
+      colorS.style.backgroundColor = "red";
+      colorS.style.color = "white";
+      }
+      );
+
+
   }
 
 pxCount.innerHTML += slider.value;
@@ -28,76 +54,114 @@ slider.oninput = function() {
   createGrid(this.value);
 }
 
-function changeBrush(e){
-  //Reset selected color
-  for( let i = 0; i < colorSelection.length; i++)
-  { 
-    colorSelection[i].style.color = "white";
-    colorSelection[i].style.backgroundColor = "red";
 
-  }
 
-  //Highlight new selected color
-  this.style.color = this.innerHTML;
-  this.style.backgroundColor = 'white';
-  document.querySelectorAll('.box').forEach(element => {
-    element.addEventListener('mouseover',event =>{
-      element.style.backgroundColor = this.innerHTML; 
-    })
-      
-  });
-  
-}
 
-function grayscaleGrid(){
-  document.querySelectorAll('.box').forEach(element => {
-      element.addEventListener('mouseover',event =>{
-        element.style.backgroundColor = "black"; 
 
-        let bgColor = element.style.getPropertyValue("background-color");
-        console.log(bgColor);
-       /* red = Math.floor(Math.random() * 256);
-        green = Math.floor(Math.random() * 256);
-        blue = Math.floor(Math.random() * 256);
-        element.style.backgroundColor = "rgb("+red+","+ green+","+ blue+")"; */
+
+function createGrid(side){
+    container.style.setProperty('--grid-rows', side);
+    container.style.setProperty('--grid-cols', side);
+
+    for (i = 0; i < (side * side); i++) 
+      {
+        let cell = document.createElement("div");
+        cell.className = 'box';
+        container.appendChild(cell);
+        
+      }
+      document.querySelectorAll('.box').forEach(element => {
+        
+        element.addEventListener('mouseover',event =>{
+          element.style.backgroundColor = checkChoice( element.style.backgroundColor);
         })
+        
+      });
+      checkToggle();
       
-    });
 
+    }
+function grayscaleGrid(){
+  colorIndicator.style.backgroundColor = "gray";
+  userChoice = "grayscale";
   }
-
-
 
 function rainbowGrid() {
-  let red, green, blue;
-
-
-  document.querySelectorAll('.box').forEach(element => {
-      element.addEventListener('mouseover',event =>{
-        red = Math.floor(Math.random() * 256);
-        green = Math.floor(Math.random() * 256);
-        blue = Math.floor(Math.random() * 256);
-        element.style.backgroundColor = "rgb("+red+","+ green+","+ blue+")";        })
-      
-    });
+  colorIndicator.style.backgroundColor ="rgb(125, 136, 72)"; 
+  userChoice = "rainbow";   
+}
     
-     
-}
-
-function eraserBrush(){
-  document.querySelectorAll('.box').forEach(element => {
-    element.addEventListener('mouseover',event =>{
-      element.style.backgroundColor = "white";})
-
-  });
-  
+function eraserBrush(){   
+  colorIndicator.style.backgroundColor = "white";
+  userChoice = "white";
 
 }
+
 function clearGrid() {
   document.querySelectorAll('.box').forEach(element => {
       element.style.backgroundColor = "white";
-     });
+      });
 }
+
+function toggleGridlines(){
+  console.log(toggleFlag);
+  toggleFlag = !toggleFlag;
+  checkToggle();
+}
+  
+function checkToggle(){
+  if (toggleFlag){
+
+    document.querySelectorAll('.box').forEach(element => {
+    element.style.border = "0.5px solid rgba(252, 162, 162, 0.5)";
+    });
+  }
+  else {
+    document.querySelectorAll('.box').forEach(element => {
+      element.style.border = "";
+    });
+
+  }
+}
+
+function checkChoice(bgColor){
+  switch(userChoice){
+    case "rainbow":
+       let red, green, blue;
+        red = Math.floor(Math.random() * 256);
+        green = Math.floor(Math.random() * 256);
+        blue = Math.floor(Math.random() * 256);
+        return "rgb("+red+","+ green+","+ blue+")"; 
+    case "grayscale":
+      if(bgColor.match(/rgba/))
+      {
+        let currentOpacity = bgColor.slice(-4, -1);
+        let newOpacity ;
+        if(currentOpacity < 0.9){
+          newOpacity = Number(currentOpacity) + 0.1; 
+        }
+        else{
+         newOpacity = Number(currentOpacity);
+        }
+        return "rgba(0, 0, 0,"+newOpacity+")";
+
+      
+      }
+      else{
+        return"rgba(0, 0, 0, 0.1)"; 
+      }
+      default:
+        return userChoice;
+
+
+   
+    
+  }
+
+}
+
+
+
 
 
 /*
@@ -122,40 +186,9 @@ function resetGrid() {
   }  
 
 }
+
+
 */
-
-
-
-
-function createGrid(side){
-    container.style.setProperty('--grid-rows', side);
-    container.style.setProperty('--grid-cols', side);
-
-    for (i = 0; i < (side * side); i++) 
-      {
-        let cell = document.createElement("div");
-       
-        //cell.innerText = (i + 1);
-        cell.className = 'box';
-        container.appendChild(cell);
-        
-      }
-      document.querySelectorAll('.box').forEach(element => {
-        element.addEventListener('mouseover',event =>{
-          element.style.backgroundColor = "black";
-        })
-        
-      });
-      
-
-    }
-
-
-
-
-
-
-
 
 
 
